3671e9bab4ff74b8323a76eaf19e1bcee2c3bb4c
Alex Shkor <alex.shkor@gmail.com>
2013-07-08 19:06:43 +0300

    inital commit

---
+++ src/RedirectPermanent/RedirectPermanent/Global.asax.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Configuration;
+using System.Web.Security;
+using System.Web.SessionState;
+
+namespace RedirectPermanent
+{
+    public class Global : System.Web.HttpApplication
+    {
+        protected void Application_BeginRequest(object sender, EventArgs e)
+        {
+            Response.RedirectPermanent(WebConfigurationManager.AppSettings["url"]);
+        }
+    }
+}
